85c80402a27e0f7df3d51de3a28c8f8244182e31
CatFromH3ll <140316561+CatFromH3ll@users.noreply.github.com>
2026-06-01 22:41:03 +0300

    menu

---
+++ MobileEndlessShooter/Assets/Scripts/Ui/UIControler.cs
@@ -0,0 +1,42 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIControler : MonoBehaviour
+{
+   public void StartGame()
+   {
+      SceneManager.LoadScene(1);
+      Time.timeScale = 1;
+   }
+
+   public void QuitGame()
+   {
+      #if UNITY_EDITOR
+      UnityEditor.EditorApplication.isPlaying = false;
+      #else
+      Application.Quit();
+      #endif
+   }
+
+   public void RestartGame()
+   {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+      Time.timeScale = 1;
+   }
+
+   public void PauseGame()
+   {
+      Time.timeScale = 0;
+   }
+
+   public void ResumeGame()
+   {
+      Time.timeScale = 1;
+   }
+
+   public void LoadMenu()
+   {
+      SceneManager.LoadScene(0);
+   }
+}
